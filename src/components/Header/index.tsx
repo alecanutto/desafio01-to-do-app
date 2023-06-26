472a5f3267a78db0fdd5e0538b2476e4994d653a
Alessandra Canuto <ale.canutto@gmail.com>
2023-06-26 20:38:01 -0300

    feat: add form

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { PlusCircle } from "@phosphor-icons/react";
 import todoLogo from "../../assets/todo-logo.svg";
 
 import styles from "./header.module.css";
@@ -6,6 +7,14 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <img src={todoLogo} />
+
+      <form className={styles.newTaskForm}>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button>
+          Criar
+          <PlusCircle size={20} />
+        </button>
+      </form>
     </header>
   )
 }
